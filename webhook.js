346c966c86123350e082897606e3412c88ce51dc
Pivertg <gamingpivert@gmail.com>
2025-07-01 15:55:31 +0200

    Add files via upload

---
+++ webhook.js
@@ -0,0 +1,3 @@
+// Fichier séparé pour stocker le webhook Discord
+// ATTENTION : ce n'est pas sécurisé côté client, mais c'est moins visible dans le code principal
+const DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1389559141348937748/1-4tprZ2WFjpjBerEAJqv_w2LzAxgLWz1aiB4wyaMy326VK58cFXwGFSF0zjJU1iTOXR";
