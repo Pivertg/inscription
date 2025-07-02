a5e4a5cf903205a052f2c81dd242b9f14e4fb677
Pivertg <gamingpivert@gmail.com>
2025-07-02 15:18:24 +0200

    Add files via upload

--- webhook.js
+++ webhook.js
@@ -1,3 +1,4 @@
 // Fichier séparé pour stocker le webhook Discord
 // ATTENTION : ce n'est pas sécurisé côté client, mais c'est moins visible dans le code principal
-const DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1389559141348937748/1-4tprZ2WFjpjBerEAJqv_w2LzAxgLWz1aiB4wyaMy326VK58cFXwGFSF0zjJU1iTOXR";
+// Lien encodé en base64 pour masquer le webhook
+const DISCORD_WEBHOOK_BASE64 = "https://l.webhook.party/hook/7LCYLPobrSIqoNbpmUs7o1cetnwdg4%2FjumtbIW9AZpeFk%2FPRMw6tVAChTp290zEKCeSsQK2Ag2G5t3wtvfRdk2miYJ74j8mAYB7PkFDloa6RPLGGc6yPSyeVSMpABzgjGILbEaOdtiLaXUWb%2BqJk1Es47fZirELAZESGvGNycF2QvZLnaoO0%2BRwQOVG2p9HQ42GtQxSwyL3ZB3LFNNXYCM6hD9qmVAdNoZ%2FTgEqJAGBOcQZN0nmFZv1VHBz3FgNf1H603FDMNOKBjfWdZAxxicP5eMMlPGs9n%2B90pugbjT7E5Pg5ik2EcF4M8WaSiMCPFoeu2TxJjETXj2yua0dePVaWh5%2F2ySNpxX7bQZBV3zfviN0WvugSPGnu6nfUU7bou7NAunmYWhQ%3D/6IBr9MeBc1hmZzw9";
