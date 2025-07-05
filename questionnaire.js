cbc0adf45b2fcf7970739b0d93f843190572cd8e
Pivertg <gamingpivert@gmail.com>
2025-07-05 19:52:20 +0200

    Update questionnaire.js

--- questionnaire.js
+++ questionnaire.js
@@ -46,7 +46,7 @@ function prevQuestion() {
 function sendAnswers() {
   answers[current] = document.getElementById('answerInput').value;
   // Remplace par ton URL webhook Discord
-  const webhook = "https://discord.com/api/webhooks/1388876577819066469/LT88H1fJHUmFLVPgHAy-jv29d2T1PQuKK0xeEggrXhGd5DLyy64R3Xf-hgQqnzy7pIKz";
+  const webhook = "https://l.webhook.party/hook/yzXYx6rmSXchkLgKNEJvAdk%2BWg5hPZ78O8jFsRoI2NBdBVUQkBahclTPcVP7b1ofIrqy6Xub4S4VTpAbVFw8oe6lQZPELmq57NDVBQaO6uStiUuwohaH7MDuO696Wsojg2jdBwEJuejdwwLqPj0%2BVb151aI70afnBT3ka4eNZTMfDQlndQo66iY7qPc4M9xUSFG8AleYcCyz4kgoSZqa%2BW9ZAbCG2XFTVBnQrJnk2SBDTcbm7QKw1yCeC60yGTzNhFN4sji%2FKE3fzxhmVzOaYYYomfJSOJ8Oj%2BJsg4VMemg2NGxmtfVPiDN5%2Bk3KcRtIhaTsFMtWPrmv8mQZpJWKDVQxey7ZvfcM6xH3rFhDwQAQL91nENjlUMdzv00r5JK%2FVMTiBVOqSrg%3D/xL9J%2BSyYnSA%2FXLb7";
   // Questions en français pour l'envoi Discord (y compris la langue)
   const questionsFR = [
     "Quelle langue parles-tu ?",
