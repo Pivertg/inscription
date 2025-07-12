848341bc3a669d92dd6aa2e9af2e0fd06ab0721f
Pivertg <gamingpivert@gmail.com>
2025-07-12 23:51:17 +0200

    Update questionnaire.js

--- questionnaire.js
+++ questionnaire.js
@@ -1,16 +1,15 @@
 // Liste de questions
 const questions = [
-  "Quelle langue parles-tu ?",
-  "Quel est ton pseudo discord ?",
-  "Quel est ton ton plus gros rank en classer ?",
-  "Pouquoie veux tu faire de l'e sport ?",
-  "Quelle est ton objectif en voulant rejoindre l'équipe ?",
-  "Quel est ton âge ?",
-  "As-tu déjà participé à des matchérino ? Si oui, a tu fini a quelle manche ?",
-  "As-tu des mate avec qui tu voudrais jouer ?",
-  "Quelle sont tes disponibilités pour les entraînements ?",
-  "Peut tu voc?",
-  "Sur quelle type d'appareil joue tu ?",
+        "Quelle langue parles-tu ?",
+        "Quel est ton pseudo discord ?",
+        "Quel est ton ton record (rang) en classé ?",
+        "Pourquoi veux-tu faire de l'e-sport ?",
+        "Quels sont tes objectifs en rejoignant l'équipe ?",
+        "Quel âge as-tu ?",
+        "As-tu déjà participé à des matcherino ? Si oui, à quelle manche as-tu terminé ?",
+        "Quelles sont tes disponibilités pour les entraînements ?",
+        "Peux-tu voc?",
+        "Sur quel type d'appareil joues-tu ?",
 ];
 let answers = new Array(questions.length).fill("");
 let current = 0;
